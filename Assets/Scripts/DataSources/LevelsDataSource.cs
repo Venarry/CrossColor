c1ab72bab312a52642f4523c2e16edfffe9bc0d8
Venarry <venarry@mail.ru>
2024-12-28 15:04:43 +0300

    Add json link

--- Assets/Scripts/DataSources/LevelsDataSource.cs
+++ Assets/Scripts/DataSources/LevelsDataSource.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Video;
 
@@ -8,13 +9,13 @@ public class LevelsDataSource : MonoBehaviour
     [SerializeField] private LevelData[] _levelsName;
 
     public LevelData[] Levels => _levelsName.ToArray();
-    public string[] LevelsName => _levelsName.Select(c => c.LevelName).ToArray();
+    public string[] LevelsName => _levelsName.Select(c => c.LevelFile.name).ToArray();
 }
 
 [Serializable]
 public class LevelData
 {
-    public string LevelName;
+    public DefaultAsset LevelFile;
     public Sprite Sprite;
     public VideoClip VideoClip;
     public bool IsTutorial = false;
